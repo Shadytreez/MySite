b8a5ef7d3ef966cb6e5241f29f1eb92ff8b4b836
Truman Nguyen <trumanngny@yahoo.com>
2021-05-28 21:00:22 -0400

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,33 +1,13 @@
-    <?php
-    $errors = '';
-    $myemail = 'truman.nguyen@lc.cuny.edu';//<-----Put Your email address here.
-    if(empty($_POST['name'])  ||
-       empty($_POST['email']) ||
-       empty($_POST['message']))
-    {
-        $errors .= "\n Error: all fields are required";
-    }
+<?php
+if(isset($_POST['submit'])){
     $name = $_POST['name'];
-    $email_address = $_POST['email'];
-    $message = $_POST['message'];
-    if (!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-    $email_address))
-    {
-        $errors .= "\n Error: Invalid email address";
-    }
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_Post['message'];
+    $mailTo = "truman.nguyen@lc.cuny.edu";
+    $header = "From: ".$mailFrom;
+    $txt = "You got a email from ".$name.".\n\n".$message;
     
-    if( empty($errors))
-    {
-    $to = $myemail;
-    $email_subject = "Contact form submission: $name";
-    $email_body = "You have received a new message. ".
-    " Here are the details:\n Name: $name \n ".
-    "Email: $email_address\n Message \n $message";
-    $headers = "From: $myemail\n";
-    $headers .= "Reply-To: $email_address";
-    mail($to,$email_subject,$email_body,$headers);
-    //redirect to the 'thank you' page
-    header('Location: contact-form-thank-you.html');
-    }
-    ?>
+    mail($mailTo,$subject,$txt,$header)
+    header("Location: contact.html?mailsend");    
+}
